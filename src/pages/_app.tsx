3abb8d8ebf6c53cfa51773eb9d63e1dd6f1f83fa
Kien Ngo <kenseverus@gmail.com>
2023-11-02 17:15:00 -0400

    Update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    // <ThirdwebProvider
+    //   activeChain="ethereum"
+    //   clientId="408c30bea260697a97e85d9b9294acf1"
+    // >
+      <Component {...pageProps} />
+    // </ThirdwebProvider>
+  );
 }
